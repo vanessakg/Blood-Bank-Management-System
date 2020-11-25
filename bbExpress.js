78872e5e6b8b7a1fb11158d1c47b16ec9f53a5ed
vanessakg <vanessagill94@hotmail.com>
2020-11-25 15:44:53 -0600

    Connect to DB and Created patient page

--- bbExpress.js
+++ bbExpress.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
 
 app.set('view engine', 'pug' );
 app.use( express.static('public'));
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "csc4500",
+    password: "gabby2020",
+    database: "blood_bank",
+    port: 3306
+})
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected!");
+});
 
 app.get('/bloodbankhome', function(req, res){
     res.render('startPage')
@@ -10,5 +25,20 @@ app.get('/bloodbankhome', function(req, res){
 app.get('/registerDonor', function(req, res){
     res.render('createDonor')
 });
+app.get('/registerPatient', function(req, res){
+    res.render('createPatient')
+});
 
-app.listen(3000);
+app.post('/sendmyProfile', function(req, res){
+   console.log(req.body);
+   var sql = `INSERT INTO donor(donor_id, d_name, d_bloodType, d_age, d_phone, d_gender, d_email, d_address) 
+              VALUES ('"+req.body.donor_id+"', '"+req.body.d_name+"', '"+req.body.d_bloodType, '"+req.body.d_age+"','"+req.body.d_phone+"','"+req.body.d_gender+"','"+req.body.d_email+"', '"+req.body.d_address+"'`;
+
+    con.query(sql, function(err){
+        if(err) throw err;
+        console.log("Record Inserted to database.");
+    });
+    res.send("Your information has been submitted.");
+});
+
+app.listen(3333);
